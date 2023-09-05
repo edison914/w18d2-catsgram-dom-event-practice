8596bec0104c6313029bd0375afe1871449842e8
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-09-05 17:45:38 -0400

    completed part1

--- public/index.js
+++ public/index.js
@@ -1,5 +1,23 @@
 // Your code here
-window.onload = () => {
-    
+window.onload = async () => {
+    try {
+        const newHeader = document.createElement('h1')
+        newHeader.innerHTML = 'Kitten Pic';
+        document.body.appendChild(newHeader)
+
+        const res = await fetch ('https://api.thecatapi.com/v1/images/search');
+
+        const data = await res.json()
+        console.log(data)
+        const url = data[0].url;
+        console.log(url)
+
+        const newPic = document.createElement('img');
+        newPic.src = url;
+        newPic.alt = "Kitten Pic";
+        document.body.appendChild(newPic);
+
+    } catch (error) {
+        console.error(`Error retriving Cat Picture`, error)
+    }
 }
-console.log("Passed")
