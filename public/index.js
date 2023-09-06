7598ef252bc9d84078efe8ecdffa7ef2b90039b4
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-09-05 20:15:53 -0400

    solved

--- public/index.js
+++ public/index.js
@@ -1,26 +1,68 @@
 // Your code here
 window.onload = async () => {
     const newHeader = document.createElement('h1')
+    newHeader.innerHTML = 'Kitten Pic';
     const newPic = document.createElement('img');
     const newImgButton = document.createElement('button')
+    const upvoteButton = document.createElement('button')
+    const downvoteButton = document.createElement('button')
+    const popScoreText = document.createElement('div');
+    const divforVote = document.createElement('div');
+
+
+    const commentInput = document.createElement('input');
+    commentInput.placeholder = 'Enter something';
+    const commentSubmit = document.createElement('button');
+    commentSubmit.textContent = 'Submit comment'
+    const commentUl= document.createElement('ul')
+
+
+    let popScore;
 
     const fetchNewCatImg = async () => {
         try {
-
-            newHeader.innerHTML = 'Kitten Pic';
             const res = await fetch ('https://api.thecatapi.com/v1/images/search');
 
             const data = await res.json()
-            console.log(data)
+            //console.log(data)
             const url = data[0].url;
             console.log(url)
 
             newPic.src = url;
             newPic.alt = "Kitten Pic";
 
+            popScore = 0;
+            updateScore();
+            commentInput.value = ''
         } catch (error) {
             console.error(`Error retriving Cat Picture`, error)
         }
+    }
+
+    const upVote = () => {
+        popScore++;
+        console.log(popScore)
+        updateScore()
+    }
+
+    const downVote = () => {
+        popScore--;
+        updateScore()
+    }
+
+    const updateScore = () => {
+        popScoreText.innerHTML = `Popularity Score: ${popScore}`
+    }
+
+    const addedComment = () => {
+        const text = commentInput.value;
+        if (text !== '') {
+            const commentLi = document.createElement('li');
+            commentLi.textContent = text;
+            commentUl.appendChild(commentLi);
+            commentInput.value = ''
+        }
+
     }
 
     fetchNewCatImg();
@@ -28,7 +70,24 @@ window.onload = async () => {
     newImgButton.textContent = "Click me for a new cat!"
     newImgButton.addEventListener('click', fetchNewCatImg)
 
+    upvoteButton.textContent = "Upvote"
+    upvoteButton.addEventListener('click', upVote)
+
+    downvoteButton.textContent = "Downvote"
+    downvoteButton.addEventListener('click', downVote)
+
+    commentSubmit.addEventListener('click', addedComment)
+
     document.body.appendChild(newHeader);
     document.body.appendChild(newPic);
     document.body.appendChild(newImgButton);
+    document.body.appendChild(popScoreText);
+    document.body.appendChild(divforVote);
+    divforVote.appendChild(upvoteButton);
+    divforVote.appendChild(downvoteButton);
+
+    document.body.appendChild(commentInput);
+    document.body.appendChild(commentSubmit);
+    document.body.appendChild(commentUl);
+
 }
